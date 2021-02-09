db1e3fee022b74b112965a0c78c6fb0193d9357c
Nicklas Eriksson <salk1n@hotmail.com>
2021-02-09 11:19:23 +0100

    Can display all members

---
+++ Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FamilyTree
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string fullName { get; set; }
+        public int yearOfBirth { get; set; }
+        public string placeOfBirth { get; set; }
+        public int motherId { get; set; }
+        public int fatherId { get; set; }
+        public int yearOfDeath { get; set; }
+        public string placeOfDeath { get; set; }
+    }
+}
